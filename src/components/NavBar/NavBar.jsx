903b6bec5ed3fc2418b2c3bdd0b30bad744f3f8d
Sahil Patel <patelsaho@gmail.com>
2021-02-22 18:56:39 -0500

    Added content to Navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,46 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Router>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Sahil Patel
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse text-right" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/portfolio">
+                  Portfolio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Router>
+    </nav>
+  );
+};
+
+export default Navbar;
