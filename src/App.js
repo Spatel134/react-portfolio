b590fa2582fde64c9935aed639546b1d3f77b411
Sahil Patel <patelsaho@gmail.com>
2021-02-22 20:42:04 -0500

    Added style and updated routes on App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./containers/Home/Home";
+import About from "./containers/About/About";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contact from "./containers/Contact/Contact";
+import Footer from "./components/Footer/Footer"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-    <h1>Testing</h1>
+      <Router basename="/">
+        <Header />
+        <main className="page">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            <Route path="*" component={Home} />
+          </Switch>
+        </main>
+      </Router>
+      <Footer />
     </div>
   );
 }
